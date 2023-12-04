4279bede1d78cf8d6e12afe29bd3bdcf1788947e
DonAntonio04 <berrellezantonio04@gmail.com>
2023-12-03 22:38:33 -0700

    primero

---
+++ ArreglosU1_Repaso/Arreglos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArreglosU1_Repaso
+{
+    public class Arreglos
+    {
+        //como crear una matriz
+        string[] persona = new string[10];
+
+        public void InsertarEnPosicion(int posicion, string valor)
+        {
+            if (posicion < 0 || posicion >= persona.Length)
+            {
+                Console.WriteLine("Posición inválida. Debe estar entre 0 y 9.");
+                return;
+            }
+
+            persona[posicion] = valor;
+
+            Console.WriteLine($"El valor '{valor}' se ha insertado correctamente en la posición {posicion}.");
+        }
+
+        public void MostrarDatosIngresados()
+        {
+            Console.WriteLine("\nDatos ingresados:");
+
+            for (int i = 0; i < persona.Length; i++)
+            {
+                Console.WriteLine($"Posición {i}: {persona[i]}");
+            }
+        }
+
+    }
+}
---
+++ ArreglosU1_Repaso/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ArreglosU1_Repaso
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Arreglos persona = new Arreglos();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese un valor:");
+                string valor = Console.ReadLine();
+
+                Console.WriteLine("Ingrese la posición del 0 al 9:");
+                int posicion = Convert.ToInt32(Console.ReadLine());
+
+                persona.InsertarEnPosicion(posicion, valor);
+            }
+
+            // Mostrar los datos ingresados en la matriz
+            persona.MostrarDatosIngresados();
+
+            Console.ReadKey();
+        }
+    }
+}
