510fa3c16112db842b42e80ca4f19f55c81bd443
cono200 <tatattian04@gmail.com>
2023-12-06 14:01:04 -0700

    escuela

--- ArreglosU1_Repaso/Arreglos.cs
+++ ArreglosU1_Repaso/Arreglos.cs
@@ -9,12 +9,12 @@ namespace ArreglosU1_Repaso
     public class Arreglos
     {
         //como crear una matriz
-        Persona[] persona = new Persona[3];
+        Persona[] persona = new Persona[5];
         public void InsertarEnPosicion(int posicion, Persona valor)
         {
             if (posicion < - 1 || posicion >= persona.Length)
             {
-                Console.WriteLine("Posición inválida. Debe estar entre 0 y 9.");
+                Console.WriteLine("Posición inválida. Debe estar entre 0 y 4.");
                 return;
             }
 
@@ -28,7 +28,7 @@ namespace ArreglosU1_Repaso
             {
                 if (persona[i] != null && persona[i].Nombre.Equals(nombreBuscado))
                 {
-                    Console.WriteLine($"La persona con el nombre '{nombreBuscado}' se encuentra en la posición {i}.");
+                    Console.WriteLine($"La persona con el nombre '{nombreBuscado}' se encuentra en la posición {i + 1}.");
                     return persona[i];
                 }
             }
@@ -61,23 +61,10 @@ namespace ArreglosU1_Repaso
             Console.WriteLine("Los datos ingresados son");
             for(int i = 0; i < persona.Length; i++)
             {
-                Console.WriteLine($"Posición: {persona [i] }");
+                Console.WriteLine($"Posición {i + 1 }: {persona [i] }");
             }
         }
-        public void OrdenarEdadesDescendente()
-        {
-            // Filtrar elementos no nulos y ordenar por edad de manera descendente
-            var edadesOrdenadas = persona
-                .Where(p => p != null)
-                .OrderByDescending(p => p.Edad)
-                .ToArray();
-
-            // Reemplazar el arreglo original con el arreglo ordenado
-            Array.Copy(edadesOrdenadas, persona, edadesOrdenadas.Length);
-
-            Console.WriteLine("Edades ordenadas de manera descendente:");
-            MostrarDatos(); // Puedes ajustar este método según tus necesidades
-        }
+  
 
     }
 }
--- ArreglosU1_Repaso/Program.cs
+++ ArreglosU1_Repaso/Program.cs
@@ -7,19 +7,35 @@ namespace ArreglosU1_Repaso
         {
             Arreglos arreglo = new Arreglos();
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 1; i++)
             {
                 Console.WriteLine("Ingrese el nombre de la persona:");
                 string nombre = Console.ReadLine();
+
+                for (int z = 0; z < nombre.Length; i++)
+                {
+                    if (!Char.IsLetter(nombre[i]))
+                    {
+                        Console.WriteLine("El nombre solo puede contener letras.");
+                        return;
+                    }
+                }
+
+
+
                 Console.WriteLine("Ingrese la edad de la persona");
-                int edad = Convert.ToInt32(Console.ReadLine());
+                    int edad = Convert.ToInt32(Console.ReadLine());
 
-               Persona insertarPersona = new Persona(nombre, edad);
+                    Persona insertarPersona = new Persona(nombre, edad);
 
-                Console.WriteLine("Ingrese la posición en la que desea insertar los datos:");
-                int posicionInsercion = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingrese la posición en la que desea insertar los datos entre 0 y 4:");
+                    int posicionInsercion = Convert.ToInt32(Console.ReadLine());
 
-                arreglo.InsertarEnPosicion(posicionInsercion, insertarPersona);
+                    arreglo.InsertarEnPosicion(posicionInsercion, insertarPersona);
+                
+               
+
+             
                 arreglo.MostrarDatos();
 
                 Console.WriteLine("Ingrese un nombre a buscar");
@@ -28,7 +44,7 @@ namespace ArreglosU1_Repaso
                 arreglo.BuscarPorNombre(nombre2);
            
 
-                Console.WriteLine("Ingresa una posicion que deseas modificar");
+                Console.WriteLine("Ingresa una posicion que deseas modificar entre el 0 y 4");
                 int modificar = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Ingresa el nombre");
@@ -41,7 +57,7 @@ namespace ArreglosU1_Repaso
                 arreglo.ModificarPosicion(modificar,insertarpersona);
 
                 arreglo.MostrarDatos();
-                arreglo.OrdenarEdadesDescendente();
+                //arreglo.OrdenarEdadesDescendente();
                 Console.WriteLine("Ingresa una posición ha eliminar");
                 int eliminar = Convert.ToInt32(Console.ReadLine());
 
